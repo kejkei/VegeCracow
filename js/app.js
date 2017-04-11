7c0b0805d4f050398a69d4ee7fd4d988a7c525b5
Kamila Treszczotko <kamila.treszczotko@gmail.com>
2017-04-11 17:02:49 +0200

    further changes

--- js/app.js
+++ js/app.js
@@ -2,11 +2,47 @@ $(function(){
     console.log("dom");
     
  
+//adding a 'on click' to the vegan/vegetarian/friendly buttons
     
     
+    var $slideshow = $(".cb-slideshow");
+    var $fixedDiv = $(".fixed");
+    var $menu = $("section").find(".menu");
+    
+    var $veganBtn = $(".vegan").find("img");
+    var $vegetarianBtn = $('.vegetarian').find("img");
+    var $friendlyBtn = $('.friendly').find("img");
+    var $section = $("section");
     
     
+    //console.log($veganBtn, $header, $slideshow, $fixedDiv);
     
+    $veganBtn.on("click", function(){
+        $slideshow.slideUp(2000);
+        $fixedDiv.slideUp(2000);
+        $menu.addClass('changeDisplay').delay(3000);
+        $section.addClass('changeDisplay').delay(3000);
+        
+    });
     
+    $vegetarianBtn.on("click", function(){
+        $slideshow.slideUp(2000);
+        $fixedDiv.slideUp(2000);
+        $menu.addClass('changeDisplay').delay(3000);
+        $section.addClass('changeDisplay').delay(3000);
+       
+    });
+    
+     $friendlyBtn.on("click", function(){
+        $slideshow.slideUp(2000);
+        $fixedDiv.slideUp(2000);
+        $menu.addClass('changeDisplay').delay(3000);
+        $section.addClass('changeDisplay').delay(3000);
+    });
+    
+      
+
+
+
     
 });
