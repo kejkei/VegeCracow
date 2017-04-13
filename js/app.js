0ef4b8e0df56a23404454052efcd3e4e3196a0da
Kamila Treszczotko <kamila.treszczotko@gmail.com>
2017-04-14 01:40:29 +0200

    one before final commit

--- js/app.js
+++ js/app.js
@@ -1,115 +1,120 @@
-$(function(){
-    console.log("dom");
-    
- 
-//adding a 'on click' to the vegan/vegetarian/friendly buttons
-    
-    
-    var $slideshow = $(".cb-slideshow");
-    var $fixedDiv = $(".fixed");
-    var $menu = $("section").find(".menu");
-    
-    var $veganBtn = $(".vegan").find("img");
-    var $vegetarianBtn = $('.vegetarian').find("img");
-    var $friendlyBtn = $('.friendly').find("img");
-    var $section = $("section");
-    var $map = $("#map");
-    
-    var currentType = "";
-    
-    
-    //console.log($veganBtn, $header, $slideshow, $fixedDiv);
-    
-    $veganBtn.on("click", function(){
-        currentType = "vegan";
-        loading();
-        $slideshow.slideUp(2000);
-        $fixedDiv.slideUp(2000, function(){
-            $map.removeClass("no-display");
-            google.maps.event.trigger(map, 'resize');
-        });
-        $menu.addClass('changeDisplay').delay(3000);
-        $section.addClass('changeDisplay').delay(3000);
-        
-    });
-    
-    $vegetarianBtn.on("click", function(){
-        currentType = "vegetarian";
-        loading();
-        $map.removeClass("no-display").delay(2000);
-        $slideshow.slideUp(2000);
-        $fixedDiv.slideUp(2000, function(){
-            //initMap();
-            google.maps.event.trigger(map, 'resize');
-        });
-        $menu.addClass('changeDisplay').delay(3000);
-        $section.addClass('changeDisplay').delay(3000);
-       
-    });
-    
-     $friendlyBtn.on("click", function(){
-        currentType = "vege-friendly";
-        loading();
-        $map.removeClass("no-display").delay(2000);
-        $slideshow.slideUp(2000);
-        $fixedDiv.slideUp(2000, function(){
-            //initMap();
-            google.maps.event.trigger(map, 'resize');
-        });
-        $menu.addClass('changeDisplay').delay(3000);
-        $section.addClass('changeDisplay').delay(3000);
-    });
-    
-      
-
-var placesUrl = "data.json";
-var itemsLoading1 = $(".restaurants");
-var itemsLoading2 = $(".stores");
-
-    function insertContent(places){
-        $.each(places, function(index, content){ 
-        if (content.category.includes(currentType)) {
-        
-        
-        var $div = $("<div>").addClass("item");
-        var $title = $("<h3>").text(content.name);
-        var $address = $("<h4>").text(content.address);
-        $address.attr("data-icon", content.icon);
-            console.log('h4', content.icon);
-        var $image = $("<img>").attr("src", "img/" + content.image);
-        var $desc = $("<p>").text(content.desc);
-            
-    $div.append($title).append($address).append($image).append($desc);
-              
-       
-        if (content.type === "store") {
-       	   itemsLoading2.append($div);
-       	} else if (content.type == "restaurant") {
-       	    itemsLoading1.append($div);
-       	}
-        }
-        });
-        
-    geocodeAddress();   
-        
-    }
-    
-
-function loading(){  
-    $.ajax({
-        url: placesUrl,
-        type: "GET",
-        dataType: "json",
-        }).done(function(response){ 
-        console.log(response.places);
-        insertContent(response.places);
-        }).fail(function(error){ 
-        console.log(error)
-    });  
-    
- }
+$(function () {
+	console.log("dom");
 
 
+	//adding a 'on click' to the vegan/vegetarian/friendly buttons
 
-    
+
+	var $slideshow = $(".cb-slideshow");
+	var $fixedDiv = $(".fixed");
+	var $menu = $("section").find(".menu");
+
+	var $veganBtn = $(".vegan").find("img");
+	var $vegetarianBtn = $('.vegetarian').find("img");
+	var $friendlyBtn = $('.friendly').find("img");
+	var $section = $("section");
+	var $map = $("#map");
+	var $footer = $("footer");
+	var $legend = $("#legend");
+
+	var currentType = "";
+
+
+	//console.log($veganBtn, $header, $slideshow, $fixedDiv);
+
+	$veganBtn.on("click", function () {
+		currentType = "vegan";
+		loading();
+		$slideshow.slideUp(2000);
+		$fixedDiv.slideUp(2000, function () {
+			$footer.removeClass("no-display").delay(2000);
+			$map.removeClass("no-display").delay(2000);
+			$legend.removeClass("no-display").delay(2000);
+			google.maps.event.trigger(map, 'resize');
+		});
+		$menu.addClass('changeDisplay').delay(3000);
+		$section.addClass('changeDisplay').delay(3000);
+
+	});
+
+	$vegetarianBtn.on("click", function () {
+		currentType = "vegetarian";
+		loading();
+		$map.removeClass("no-display").delay(2000);
+		$footer.removeClass("no-display").delay(2000);
+		$legend.removeClass("no-display").delay(2000);
+		$slideshow.slideUp(2000);
+		$fixedDiv.slideUp(2000, function () {
+			//initMap();
+			google.maps.event.trigger(map, 'resize');
+		});
+		$menu.addClass('changeDisplay').delay(3000);
+		$section.addClass('changeDisplay').delay(3000);
+
+	});
+
+	$friendlyBtn.on("click", function () {
+		currentType = "vege-friendly";
+		loading();
+		$map.removeClass("no-display").delay(2000);
+		$legend.removeClass("no-display").delay(2000);
+		$footer.removeClass("no-display").delay(2000);
+		$slideshow.slideUp(2000);
+		$fixedDiv.slideUp(2000, function () {
+			//initMap();
+			google.maps.event.trigger(map, 'resize');
+		});
+		$menu.addClass('changeDisplay').delay(3000);
+		$section.addClass('changeDisplay').delay(3000);
+	});
+
+
+	var placesUrl = "data.json";
+	var itemsLoading1 = $(".restaurants");
+	var itemsLoading2 = $(".stores");
+
+	function insertContent(places) {
+		$.each(places, function (index, content) {
+			if (content.category.includes(currentType)) {
+
+
+				var $div = $("<div>").addClass("item");
+				var $title = $("<h3>").text(content.name);
+				var $address = $("<h4>").text(content.address);
+				$address.attr("data-icon", content.icon);
+				console.log('h4', content.icon);
+				var $image = $("<img>").attr("src", "img/" + content.image);
+				var $desc = $("<p>").text(content.desc);
+
+				$div.append($title).append($address).append($image).append($desc);
+
+
+				if (content.type === "store") {
+					itemsLoading2.append($div);
+				} else if (content.type == "restaurant") {
+					itemsLoading1.append($div);
+				}
+			}
+		});
+
+		geocodeAddress();
+
+	}
+
+
+	function loading() {
+		$.ajax({
+			url: placesUrl,
+			type: "GET",
+			dataType: "json",
+		}).done(function (response) {
+			console.log(response.places);
+			insertContent(response.places);
+		}).fail(function (error) {
+			console.log(error)
+		});
+
+	}
+
+
 });
--- js/map.js
+++ js/map.js
@@ -1,53 +1,57 @@
- 
+var krakow = {
+	lat: 50.06465,
+	lng: 19.94498
+};
 var map;
-function initMap() {
-    console.log('initMap');
-    //center: new google.maps.LatLng(51.5, -0.2),
-        var uluru = {lat: 50.0647, lng: 19.9450};
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 14,
-            center: uluru
-         
-        });
-      
-      }
 
-function geocodeAddress() {
-    console.log("geocodeAddress");
-    var geocoder = new google.maps.Geocoder();
-    var address = document.querySelectorAll('h4');
-    //console.log(address);
-    
-    //var markers = [];
-    for (var i=0; i< address.length; i++){
-    var text = address[i].innerText;
-    let icon = $(address[i]).data("icon");
-        console.log(icon);
-        geocoder.geocode({'address': text}, function(results, status) {
-            console.log(results, status);
-          if (status === 'OK') {
-            //console.log(map);
-            
-              map.setCenter(results[0].geometry.location);
-             //var cloud = new google.maps.InfoWindow();
-              
-              var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                animation: google.maps.Animation.DROP,
-                icon: icon, 
-                
-            });
-              
-              //marker.setMap(map);
-              //markers.push(marker);
-              
-              console.log(marker.position.lat(), marker.position.lng(), 'marker');
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
+function initMap() {
+	//var krakow = {lat: 50.06465, lng: 19.94498};
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 14,
+		center: krakow,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+
+	});
+
 }
 
-      
+function geocodeAddress() {
+	console.log("geocodeAddress");
+	var geocoder = new google.maps.Geocoder();
+	var address = document.querySelectorAll('h4');
+	//console.log(address);
+
+	//var markers = [];
+	for (var i = 0; i < address.length; i++) {
+		var text = address[i].innerText;
+		let icon = $(address[i]).data("icon");
+		console.log(icon);
+		geocoder.geocode({
+			'address': text
+		}, function (results, status) {
+			console.log(results, status);
+			if (status === 'OK') {
+				//console.log(map);
+
+				map.setCenter(results[0].geometry.location);
+				//var cloud = new google.maps.InfoWindow();
+
+				var marker = new google.maps.Marker({
+					map: map,
+					position: results[0].geometry.location,
+					animation: google.maps.Animation.DROP,
+					icon: icon,
+
+				});
+
+				//marker.setMap(map);
+				//markers.push(marker);
+
+				console.log(marker.position.lat(), marker.position.lng(), 'marker');
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		});
+	}
+}
+
